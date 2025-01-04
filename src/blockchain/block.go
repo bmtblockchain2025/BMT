7fd6f072dd5a4ab4539a0a15a48f02747576a344
BMT <bmtblockchain@gmail.com>
2025-01-04 10:11:22 +0700

    v1.3

--- src/blockchain/block.go
+++ src/blockchain/block.go
@@ -43,6 +43,7 @@ type Transaction struct {
 	Receiver  string  // Address of the receiver
 	Amount    float64 // Amount being transferred
 	Timestamp time.Time // Time of transaction creation
+	Signature string  // Digital signature for transaction integrity
 }
 
 // MiniBlock represents a mini block within the blockchain.
@@ -53,6 +54,7 @@ type MiniBlock struct {
 	CurrentSize  int            // Current size of the mini-block in bytes
 	IsFull       bool           // Indicates whether the mini-block is full
 	Key          string         // Unique key for the mini-block
+	MerkleRoot   string         // Merkle root for transactions integrity
 }
 
 // SubBlock represents a medium block containing multiple mini-blocks.
@@ -78,22 +80,31 @@ var processedTransactions sync.Map        // Map to track processed transactions
 var availableMiniBlocks []MiniBlock       // List of available mini-blocks (not full)
 var fullMiniBlocks []MiniBlock            // List of full mini-blocks
 
+// updateMiniBlockLists updates the lists of available and full mini-blocks.
+func updateMiniBlockLists(miniBlock *MiniBlock, selectedIndex int) {
+	if miniBlock.IsFull {
+		fullMiniBlocks = append(fullMiniBlocks, *miniBlock)
+		availableMiniBlocks = append(availableMiniBlocks[:selectedIndex], availableMiniBlocks[selectedIndex+1:]...)
+	} else {
+		availableMiniBlocks[selectedIndex] = *miniBlock
+	}
+}
+
 // ValidateTransactionWithFastConsensus performs fast consensus using a small group of validators.
 func ValidateTransactionWithFastConsensus(transactions []Transaction) bool {
 	if len(transactions) == 0 {
 		return false
 	}
-	requiredVotes := len(TrustedValidators) / 2 + 1
+	requiredVotes := 1
 	votes := 0
 	for validator := range TrustedValidators {
-		if rand.Float64() > 0.5 { // Simulate random vote
-			votes++
-			if votes >= requiredVotes {
-				return true
-			}
+		votes++
+		if votes >= requiredVotes {
+			break
 		}
 	}
-	return false
+	time.Sleep(10 * time.Millisecond) // Simulate 0.01 second delay for consensus
+	return true
 }
 
 // MineTransaction processes transactions and assigns them to a mini-block in a sub-block.
@@ -103,14 +114,13 @@ func MineTransaction(transactions []Transaction, mainBlock *MainBlock) (*MiniBlo
 		return nil, errors.New("transaction already processed")
 	}
 
-	mutex.Lock()
 	if len(availableMiniBlocks) == 0 {
-		mutex.Unlock()
 		return nil, errors.New("no available mini-block to record transaction")
 	}
 
-	// Randomly select an available mini-block
+	// Randomly select an available mini-block without locking the entire function
 	selectedIndex := rand.Intn(len(availableMiniBlocks))
+	mutex.Lock() // Lock only for critical section to update mini-block
 	miniBlock := &availableMiniBlocks[selectedIndex]
 	totalTransactionSize := calculateTransactionsSize(transactions)
 	if miniBlock.CurrentSize+totalTransactionSize > MaxMiniBlockSize {
@@ -122,11 +132,11 @@ func MineTransaction(transactions []Transaction, mainBlock *MainBlock) (*MiniBlo
 	miniBlock.CurrentSize += totalTransactionSize
 	if miniBlock.CurrentSize == MaxMiniBlockSize {
 		miniBlock.IsFull = true
-		fullMiniBlocks = append(fullMiniBlocks, *miniBlock)
-		availableMiniBlocks = append(availableMiniBlocks[:selectedIndex], availableMiniBlocks[selectedIndex+1:]...)
 	}
+	updateMiniBlockLists(miniBlock, selectedIndex)
 	mutex.Unlock()
 
+	miniBlock.MerkleRoot = calculateMerkleRoot(miniBlock.Transactions)
 	suffix := calculateDynamicDifficulty()
 	for {
 		miniBlock.Hash = calculateMiniBlockHash(miniBlock.Index, miniBlock.Transactions)
@@ -181,7 +191,7 @@ func calculateMiniBlockHash(index int, transactions []Transaction) string {
 func calculateTransactionsSize(transactions []Transaction) int {
 	size := 0
 	for _, tx := range transactions {
-		size += len(tx.Sender) + len(tx.Receiver) + 8 + len(tx.Timestamp.String())
+		size += len(tx.Sender) + len(tx.Receiver) + 8 + len(tx.Timestamp.String()) + len(tx.Signature)
 	}
 	return size
 }
@@ -199,6 +209,7 @@ func concatTransactions(transactions []Transaction) string {
 		builder.WriteString(tx.Receiver)
 		builder.WriteString(strconv.FormatFloat(tx.Amount, 'f', 2, 64))
 		builder.WriteString(tx.Timestamp.String())
+		builder.WriteString(tx.Signature)
 	}
 	return builder.String()
 }
@@ -214,4 +225,28 @@ func generateTransactionID(transactions []Transaction) string {
 	}
 	hash := sha256.Sum256([]byte(builder.String()))
 	return hex.EncodeToString(hash[:])
+}
+
+// calculateMerkleRoot calculates the Merkle root for a list of transactions.
+func calculateMerkleRoot(transactions []Transaction) string {
+	if len(transactions) == 0 {
+		return ""
+	}
+	var hashes []string
+	for _, tx := range transactions {
+		hashes = append(hashes, generateTransactionID([]Transaction{tx}))
+	}
+	for len(hashes) > 1 {
+		var newLevel []string
+		for i := 0; i < len(hashes); i += 2 {
+			if i+1 < len(hashes) {
+				hash := sha256.Sum256([]byte(hashes[i] + hashes[i+1]))
+				newLevel = append(newLevel, hex.EncodeToString(hash[:]))
+			} else {
+				newLevel = append(newLevel, hashes[i])
+			}
+		}
+		hashes = newLevel
+	}
+	return hashes[0]
 }
